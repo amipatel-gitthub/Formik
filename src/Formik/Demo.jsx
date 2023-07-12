b19a611c37334602821191b2e17e769f2941336d
amipatel-gitthub <amipatel-gitthub@gmail.com>
2023-07-12 18:30:15 +0530

    normal form convert into formik

--- src/Formik/Demo.jsx
+++ src/Formik/Demo.jsx
@@ -1,77 +1,82 @@
 import React from "react";
-import { Formik, useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 export default function Demo() {
+    const initialValues = {
+        id: "",
+        name: "",
+    };
 
-    const formik = useFormik({
-        initialValues : {
-            id : "",
-            name : ""
-        },
+    const onSubmit = (values) => {
+        console.log("user Data ", values);
+    };
 
-        onSubmit : values => {
-            console.log("user Data : ",formik.values);
+    const validate = (values) => {
+        let error = {};
+
+        if (!values.id) {
+            error.id = "Required";
         }
 
-    })
+        if (!values.name) {
+            error.name = "Required";
+        }
+        return error;
+    };
 
-    // console.log("formik Values : ",formik.values);
+    const validateSchema = yup.object({
+        id: yup.string().required("Required !!!"),
+        name: yup.string().required("Required !!"),
+    });
+
     return (
         <>
-        
             <div className="container">
                 <div className="mx-auto mt-4 border p-3" style={{ width: "600px" }}>
-                    <form onSubmit={formik.handleSubmit}>
-                        <h4 className="text-center">Simple Crud operation</h4>
-                        <hr />
-                        <div className="form-outline mb-2">
-                            <div className="row">
-                                <div className="col-3">
-                                    <label className="fw-bolder">User Id : </label>
-                                </div>
-                                <div className="col-9">
-                                    <input type="text" className="form-control" name="id" 
-                                    value={formik.values.id}
-                                    onChange={formik.handleChange}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="form-outline mb-2">
-                            <div className="row">
-                                <div className="col-3">
-                                    <label className="fw-bolder">User name : </label>
-                                </div>
-                                <div className="col-9">
-                                    <input type="text " className="form-control" name="name" 
-                                    value={formik.values.name}
-                                    onChange={formik.handleChange}
-                                    />
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={onSubmit}
+                        validate={validate}
+                        validateSchema={validateSchema}
+                    >
+                        <Form>
+                            <h4 className="text-center">Simple Crud operation</h4>
+                            <hr />
+                            <div className="form-outline mb-2">
+                                <div className="row">
+                                    <div className="col-3">
+                                        <label className="fw-bolder">User Id : </label>
+                                    </div>
+                                    <div className="col-9">
+                                        <Field type="number" className="form-control" name="id" />
+                                        <ErrorMessage name="id" />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="m-auto text-center">
-                            <button type="submit" className="btn btn-primary btn-block  px-3">
-                                Sign up 
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                            <div className="form-outline mb-2">
+                                <div className="row">
+                                    <div className="col-3">
+                                        <label className="fw-bolder">User name : </label>
+                                    </div>
+                                    <div className="col-9">
+                                        <Field type="text " className="form-control" name="name" />
+                                        <ErrorMessage name="name" />
+                                    </div>
+                                </div>
+                            </div>
 
-                <table className="table  table-hover table-bordered mt-5 text-center mb-3">
-                    <thead>
-                        <tr>
-                            <th className="text-secondary">User Id</th>
-                            <th className="text-secondary">User name </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
+                            <div className="m-auto text-center">
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary btn-block  px-3"
+                                >
+                                    Print
+                                </button>
+                            </div>
+                        </Form>
+                    </Formik>
+                </div>
             </div>
         </>
     );
